20b53058d15d12ca3ab4f0d248f4df0d8d476c4f
unknown <team@gmail>
2023-08-20 22:36:57 -0600

    Package.jason configuration_I added Script_prebuild and gh-pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://Cefz19.github.io/proyect_ecommer_react',
 })
